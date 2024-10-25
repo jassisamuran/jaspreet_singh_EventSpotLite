9c4b9a463ba6b8f38de5121d16d710cbb8975ec4
jassisamuran <samuran3132@gmail.com>
2024-10-26 03:41:28 +0530

    add EventCard for showing details of event in card

--- src/components/EventCard.js
+++ src/components/EventCard.js
@@ -1,11 +1,11 @@
 import React from "react";
 import {motion} from 'framer-motion'
-
+// import '../EventCard.css'
 function EventCard({event,onClick}){
     return (
         <motion.div
             className="event-card"
-            onClick={onclick}
+            onClick={onClick}
             whileHover={{scale:1.05}}
         >
             <img src={event.image} alt={event.name}/>
