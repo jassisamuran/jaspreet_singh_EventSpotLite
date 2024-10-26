04baf7be05b7fa5f48da77b1628ce93eb1037c5f
jassisamuran <samuran3132@gmail.com>
2024-10-26 20:24:26 +0530

    adding more data in data file for showing more  data

--- src/components/data.js
+++ src/components/data.js
@@ -17,60 +17,60 @@ export const events = [
     },
     {
       id: 3,
-      name: "Music Festival",
-      date: "2024-11-01",
-      location: "Mumbai",
-      description: "Enjoy a variety of music at the Music Festival!",
-      image: "https://th.bing.com/th/id/OIP.qVwQJqwVXHIL20Ote20aLAHaE8?rs=1&pid=ImgDetMain"},
+      name: "Coding Compedition",
+      date: "2024-11-010",
+      location: "Delhi",
+      description: "Enjoy compedition",
+      image: "https://th.bing.com/th/id/OIP.TS1ZD1o5Lnml5a4aJc-kbgHaE8?rs=1&pid=ImgDetMain"},
     {
       id: 4,
-      name: "Art Expo",
-      date: "2024-12-15",
-      location: "Delhi",
-      description: "Explore modern art pieces at the Art Expo.",
-      image: "https://www.artfairmag.com/wp-content/uploads/artexpo-new-york-2-1.jpg"
+      name: "College Event Ideads",
+      date: "2024-11-15",
+      location: "Chandigarh",
+      description: "Explore Things Which is next.",
+      image: "https://th.bing.com/th/id/OIP.yljzAu3TwGsJeLtEUk1McgHaF7?rs=1&pid=ImgDetMain"
     },   {
       id: 5,
-      name: "Music Festival",
-      date: "2024-11-01",
+      name: "Selling Compedition",
+      date: "2024-11-11",
       location: "Mumbai",
-      description: "Enjoy a variety of music at the Music Festival!",
-      image: "https://th.bing.com/th/id/OIP.qVwQJqwVXHIL20Ote20aLAHaE8?rs=1&pid=ImgDetMain"},
+      description: "Who ever sells more wins the compedtion.",
+      image: "https://www.oxy.edu/sites/default/files/styles/banner_image/public/landing-page/banner-images/events-homecoming_main_1440x800.jpg?itok=VUY_RRmf"},
     {
       id: 6,
-      name: "Art Expo",
-      date: "2024-12-15",
+      name: "Dj Night",
+      date: "2024-11-15",
       location: "Delhi",
-      description: "Explore modern art pieces at the Art Expo.",
-      image: "https://www.artfairmag.com/wp-content/uploads/artexpo-new-york-2-1.jpg"
+      description: "Explore all new Friends in Dj Night.",
+      image: "https://th.bing.com/th/id/OIP.2n5bWGMtuoa5ynjDUG9TggAAAA?rs=1&pid=ImgDetMain"
     },   {
       id: 7,
-      name: "Music Festival",
-      date: "2024-11-01",
-      location: "Mumbai",
-      description: "Enjoy a variety of music at the Music Festival!",
-      image: "https://th.bing.com/th/id/OIP.qVwQJqwVXHIL20Ote20aLAHaE8?rs=1&pid=ImgDetMain"},
+      name: "Tour",
+      date: "2024-11-04",
+      location: "California",
+      description: "Enjoy a variety of culture at the tour California.",
+      image: "https://th.bing.com/th/id/OIP.OPTIc9drJyM0JfwfP1F5KwHaE8?rs=1&pid=ImgDetMain"},
     {
       id: 8,
-      name: "Art Expo",
+      name: "Trip",
       date: "2024-12-15",
-      location: "Delhi",
-      description: "Explore modern art pieces at the Art Expo.",
-      image: "https://www.artfairmag.com/wp-content/uploads/artexpo-new-york-2-1.jpg"
+      location: "Goa",
+      description: "Experience Goa nights 5 days and 4 night trip with friends.",
+      image: "https://th.bing.com/th/id/OIP.FL6r-StiKPIoGxDaylYHMwHaEb?rs=1&pid=ImgDetMain"
     },   {
       id: 9,
       name: "Music Festival",
       date: "2024-11-01",
       location: "Mumbai",
       description: "Enjoy a variety of music at the Music Festival!",
-      image: "https://th.bing.com/th/id/OIP.qVwQJqwVXHIL20Ote20aLAHaE8?rs=1&pid=ImgDetMain"},
+      image: "https://marketplace.canva.com/EAFJXu7Spiw/1/0/1131w/canva-black-orange-bold-music-festival-poster-fLC8RaO0Qzc.jpg"},
     {
       id: 10,
-      name: "Art Expo",
-      date: "2024-12-15",
-      location: "Delhi",
-      description: "Explore modern art pieces at the Art Expo.",
-      image: "https://www.artfairmag.com/wp-content/uploads/artexpo-new-york-2-1.jpg"
+      name: "Modern Science",
+      date: "2024-12-30",
+      location: "Chandigarh",
+      description: "Explore modern Science in Chandigarh in upcoming days.",
+      image: "https://th.bing.com/th/id/OIP.66TjpM3HCAbaJbrdNfIPIgHaDf?rs=1&pid=ImgDetMain"
     },   {
       id: 11,
       name: "Music Festival",
